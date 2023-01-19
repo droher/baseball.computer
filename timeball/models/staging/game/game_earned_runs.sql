b463dcc433402c218ba99b14f11ee2261ddfb017
David Roher <david.roher@gmail.com>
2023-01-19 16:41:18 -0500

    Add earned run source

---
+++ timeball/models/staging/game/game_earned_runs.sql
@@ -0,0 +1,13 @@
+WITH source AS (
+      SELECT * FROM {{ source('game', 'game_earned_runs') }}
+),
+renamed as (
+    SELECT
+        game_id,
+        player_id,
+        earned_runs
+    FROM source
+)
+
+SELECT * FROM renamed
+  
